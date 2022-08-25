a8e549fedd60fb5f71d577868202ef48bfe71ce0
kvsketch <kellonv@gmail.com>
2022-08-25 17:30:10 -0600

    reworking the calculator in a new js file, start by removing inline html onclick

--- script.js
+++ script.js
@@ -12,17 +12,24 @@ function currentNumber(value) {
     displayNum.textContent = current;
   }
   display.appendChild(displayNum);
+  clear();
 }
 
 function operator(operator) {
+    if (operation.length >= 2) {
+        operation.splice(0, 2);
+        operation[0] = answer;
+      }
   switch (operator) {
     case "+":
       operation.push(Number(current));
+      console.log(operation);
       number = [];
       answer = sum();
       break;
     case "-":
       operation.push(Number(current));
+      console.log(operation);
       number = [];
       answer = difference();
       break;
@@ -37,13 +44,32 @@ function operator(operator) {
       answer = quotient();
       break;
   }
+  compute(operator)
   displayNum.textContent = answer;
-  //if operation length reaches 2, make operation array only answer left
-  if (operation.length >= 2) {
-    operation.splice(0, 2);
-    operation[0] = answer;
-  }
 }
+
+function compute(operator) {
+    const equalBtn = document.getElementById('=')
+    equalBtn.addEventListener('click', () => {
+    if (operation[0] != null && opeation [1] != null) {
+        if (operator == '+') {
+            answer = sum();
+            displayNum.textContent = answer;
+        }
+    }
+});
+}
+
+function clear() {
+    const btnClear = document.getElementById('clear');
+    btnClear.addEventListener('click', () => {
+        if (current != '' || answer != '') {
+        number = [];
+        displayNum.textContent = '';
+        };
+    })
+}
+
 
 let sum = () => operation.reduce((x, y) => x + y);
 let difference = () => operation.reduce((x, y) => x - y);
@@ -55,3 +81,4 @@ let quotient = () => operation.reduce((x, y) => x / y);
 //upon clicking operator reset the current value and store it *
 //collect new current value and output the answer if clicking operator * or = *
 //store answer and allow it to be used with an operator or clear the calculator
+//if only 1 current number clear if operation is greater than 2 allow AC after number is nothing
